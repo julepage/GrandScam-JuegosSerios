256e4bc19d62ae4be83ee0875992cddd7508885e
DiverBro <serginar@ucm.es>
2025-11-15 17:17:58 +0100

    Escena victoria con fondo

--- docs/src/escenaVictoria.js
+++ docs/src/escenaVictoria.js
@@ -4,26 +4,19 @@ export default class Victoria extends Phaser.Scene {
     }
 
     create() {
-        //CARGAR TEXTOS
-        const textos = this.cache.json.get('es');
+        //POSICION Y TAMAÑO DEL FONDO
+        this.fondo = this.add.image(0, 0, 'fondoJuego');
+        this.fondo.setScale(this.cameras.main.height / this.fondo.height);
+        this.fondo.setPosition(this.cameras.main.width / 2, this.cameras.main.height / 2);
         //FONDO TRANSLUCIDO
         const { width, height } = this.scale;
         this.add.rectangle(0, 0, width * 2, height * 2, 0x000000, 0.8).setOrigin(0);
 
-        this.volver = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'VICTORIA', {
-            fontFamily: 'Georgia, "Times New Roman", serif',
-            fontSize: '150px',
-            color: '#ff5100ff',
-            stroke: '#561b00ff',
-            strokeThickness: 6,
-            align: 'center'
-        })
-            .setInteractive({ useHandCursor: true })
-            .setScale(0.4)
-            .setDepth(1)
-            .setOrigin(0.5, 0.5);
+        this.fondoV = this.add.image(0, 0, 'fondoVictoria').setInteractive({ useHandCursor: true });
+        this.fondoV.setScale(this.cameras.main.height / this.fondo.height);
+        this.fondoV.setPosition(this.cameras.main.width / 2, this.cameras.main.height / 2);
 
-        this.volver.on('pointerdown', () => {
+        this.fondoV.on('pointerdown', () => {
             this.scene.start('menu');
         });
     }
